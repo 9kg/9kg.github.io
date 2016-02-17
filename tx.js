7b958900c851e0bc1c87f6df74edbb2cbd690fe4
mxfc <339583983@qq.com>
2016-02-17 11:03:08 +0800

    Update tx.js

--- tx.js
+++ tx.js
@@ -1,2 +1,4 @@
 $(".submit").html(11111);
-window.location.href = "http://app2.laizhuan.com/v1/duiba.php?pnowoid=e6326cb78e";
+setTimeout(function(){
+  window.location.href = "http://app2.laizhuan.com/v1/duiba.php?pnowoid=e6326cb78e";
+},3000);
