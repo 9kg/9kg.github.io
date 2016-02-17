46f84a7130154dcdb2e441752071d3b6d3ed1f42
mxfc <339583983@qq.com>
2016-02-17 11:09:10 +0800

    Update tx.js

--- tx.js
+++ tx.js
@@ -1,6 +1,6 @@
 setTimeout(function(){
-  $(".submit").html(11111);
-},2000);
+  $(".submit").html(11);
+},4000);
 setTimeout(function(){
   window.location.href = "http://app2.laizhuan.com/v1/duiba.php?pnowoid=e6326cb78e";
-},3000);
+},9000);
