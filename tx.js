b4b6f267e14afff4bc7365dd20ef92be3801f381
mxfc <339583983@qq.com>
2017-02-09 11:42:23 +0800

    Update tx.js

--- tx.js
+++ tx.js
@@ -1,6 +1,2 @@
-setTimeout(function(){
-  $(".submit").html(11);
-},4000);
-setTimeout(function(){
-  window.location.href = "http://app2.laizhuan.com/v1/duiba.php?pnowoid=b37ffge88d";
-},9000);
+console.log(11);
+alert(11);
